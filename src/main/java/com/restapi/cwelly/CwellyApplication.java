7ddd232d14374f32240fd377fc859ca65f306a0a
cwelly <potato3884@naver.com>
2023-10-07 18:50:43 +0900

    Project created

---
+++ src/main/java/com/restapi/cwelly/CwellyApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.cwelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwellyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/cwelly/CwellyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.cwelly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwellyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
