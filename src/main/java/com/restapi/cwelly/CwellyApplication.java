712bc6ccf01cf1526bf0229a5d6ca1c6da4bd0a7
cwelly <potato3884@naver.com>
2023-10-30 22:13:34 +0900

    add event logic

--- src/main/java/com/restapi/cwelly/CwellyApplication.java
+++ src/main/java/com/restapi/cwelly/CwellyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CwellyApplication {
 
+	// basePrice 와 maxPrice에 따른 다른 Event 처리할 예정
 	public static void main(String[] args) {
 		SpringApplication.run(CwellyApplication.class, args);
 	}
-
 }
